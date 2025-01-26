e1b2755c3a5c7619f267566ab8b2140a75a02ca7
cobbland <jacobdensford@proton.me>
2025-01-26 09:17:00 -0500

    Complete planning and psudocode

---
+++ script.js
@@ -0,0 +1,9 @@
+// Prompt player for input
+// Validate input
+// Generate computer choice
+// Compare player input against computer choice
+// Determine the winner
+// Declare the winner
+// Increment winning count
+// Declare winning count
+// Repeat
